fbba08a39dbf875ca7faa2913aa4a5491308eac1
Paolo Chang <pchang@architech.ca>
2022-04-22 15:12:03 -0400

    Remove CORS

--- src/main/java/io/spring/RealWorldApplication.java
+++ src/main/java/io/spring/RealWorldApplication.java
@@ -2,24 +2,11 @@ package io.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RealWorldApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RealWorldApplication.class, args);
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("./**").allowedOrigins("https://angular-rachdemo.azurewebsites.net");
-      }
-    };
   }
 }
