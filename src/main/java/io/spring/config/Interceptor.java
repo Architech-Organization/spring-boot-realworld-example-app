2e3089db2a6605a3412bc9b8132a661606d82e18
Paolo Chang <pchang@architech.ca>
2022-05-24 15:42:23 -0400

    Add missing annotations

--- src/main/java/io/spring/config/Interceptor.java
+++ src/main/java/io/spring/config/Interceptor.java
@@ -1,13 +1,17 @@
 package io.spring.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
+@Component
 public class Interceptor implements HandlerInterceptor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Interceptor.class);
