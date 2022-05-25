465072c5dfabd0104660303ef4a6340537a15767
Paolo Chang <pchang@architech.ca>
2022-05-25 11:38:55 -0400

    Put the logger back

--- src/main/java/io/spring/config/Interceptor.java
+++ src/main/java/io/spring/config/Interceptor.java
@@ -2,17 +2,24 @@ package io.spring.config;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Slf4j
 @Component
 public class Interceptor implements HandlerInterceptor {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Interceptor.class);
 
   @Override
   public void afterCompletion(
       HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
       throws Exception {
-    System.out.println("print out afterCompletion interceptor");
+    final String requestMethod = request.getMethod();
+    final String requestURI = request.getRequestURI();
+    LOGGER.info("[" + requestMethod + "] " + requestURI + " " + response.getStatus());
     HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
   }
 }
